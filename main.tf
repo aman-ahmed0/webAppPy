b35609fe84e6990e098a9c6e06c4b8c9d2c73547
ansible_wsl_aman <ansible_wsl_aman@gmail.com>
2025-02-23 14:01:22 +0200

    terraform.tfvars+var.tf

--- main.tf
+++ main.tf
@@ -10,48 +10,96 @@ terraform {
 provider "azurerm" {
   features {}
 
-  subscription_id = var.azure_subscription_id  # Retrieved from Azure
-  client_id       = var.azure_client_id        # Matches "appId"
-  client_secret   = var.azure_client_secret    # Matches "password"
-  tenant_id       = var.azure_tenant_id        # Matches "tenant"
+  subscription_id = var.azure_subscription_id
+  client_id       = var.azure_client_id
+  client_secret   = var.azure_client_secret
+  tenant_id       = var.azure_tenant_id
 }
 
-
-resource "azurerm_resource_group" "devopsproject_rg" {
-  name     = "devops-proj-rg"
-  location = "East US"
-}
-
-resource "azurerm_container_group" "devops_app" {
-  depends_on = [random_string.suffix]
-  
-  name                = "devops-app-container"
-  location            = azurerm_resource_group.devops_rg.location
-  resource_group_name = azurerm_resource_group.devops_rg.name
-  os_type             = "Linux"
-
-  container {
-    name   = "devops-app"
-    image  = "ahmedaman0/webapppy:latest"
-    cpu    = "0.5"
-    memory = "1.5"
-
-    ports {
-      port     = 5000
-      protocol = "TCP"
-    }
-  }
-
-  dns_name_label = "devops-app-${random_string.suffix.result}"
-}
-
+# Generate a random string to make names unique
 resource "random_string" "suffix" {
-  length  = 4
+  length  = 6
   special = false
   upper   = false
 }
 
+# Resource Group with unique name
+resource "azurerm_resource_group" "devops_rg" {
+  name     = "devops-proj-rg-${random_string.suffix.result}"
+  location = "East US"
+}
 
-output "container_public_ip" {
-  value = azurerm_container_group.devops_app.ip_address
+# Virtual Network
+resource "azurerm_virtual_network" "devops_vnet" {
+  name                = "devops-vnet-${random_string.suffix.result}"
+  location            = azurerm_resource_group.devops_rg.location
+  resource_group_name = azurerm_resource_group.devops_rg.name
+  address_space       = ["10.0.0.0/16"]
+}
+
+# Subnet
+resource "azurerm_subnet" "devops_subnet" {
+  name                 = "devops-subnet-${random_string.suffix.result}"
+  resource_group_name  = azurerm_resource_group.devops_rg.name
+  virtual_network_name = azurerm_virtual_network.devops_vnet.name
+  address_prefixes     = ["10.0.1.0/24"]
+}
+
+# Public IP
+resource "azurerm_public_ip" "devops_public_ip" {
+  name                = "devops-vm-ip-${random_string.suffix.result}"
+  location            = azurerm_resource_group.devops_rg.location
+  resource_group_name = azurerm_resource_group.devops_rg.name
+  allocation_method   = "Static"
+}
+
+# Network Interface
+resource "azurerm_network_interface" "devops_nic" {
+  name                = "devops-nic-${random_string.suffix.result}"
+  location            = azurerm_resource_group.devops_rg.location
+  resource_group_name = azurerm_resource_group.devops_rg.name
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.devops_subnet.id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = azurerm_public_ip.devops_public_ip.id
+  }
+}
+
+# Virtual Machine
+resource "azurerm_linux_virtual_machine" "devops_vm" {
+  name                = "devops-vm-${random_string.suffix.result}"
+  resource_group_name = azurerm_resource_group.devops_rg.name
+  location            = azurerm_resource_group.devops_rg.location
+  size                = "Standard_B2s"
+  admin_username      = "azureuser"
+  network_interface_ids = [azurerm_network_interface.devops_nic.id]
+
+  admin_ssh_key {
+    username   = "azureuser"
+    public_key = var.ssh_public_key
+  }
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "UbuntuServer"
+    sku       = "16.04-LTS"
+    version   = "16.04.202011120"
+  }
+}
+
+# Output VM Public IP
+output "vm_public_ip" {
+  value = azurerm_public_ip.devops_public_ip.ip_address
+}
+
+# Output Resource Group Name
+output "resource_group_name" {
+  value = azurerm_resource_group.devops_rg.name
 }
--- variables.tf
+++ variables.tf
@@ -1,4 +1,25 @@
-variable "azure_subscription_id" {}
-variable "azure_client_id" {}
-variable "azure_client_secret" {}
-variable "azure_tenant_id" {}
+variable "azure_subscription_id" {
+  description = "Azure Subscription ID"
+  type        = string
+}
+
+variable "azure_client_id" {
+  description = "Azure Client ID (Service Principal)"
+  type        = string
+}
+
+variable "azure_client_secret" {
+  description = "Azure Client Secret (Service Principal)"
+  type        = string
+  sensitive   = true
+}
+
+variable "azure_tenant_id" {
+  description = "Azure Tenant ID"
+  type        = string
+}
+
+variable "ssh_public_key" {
+  description = "SSH Public Key for VM Authentication"
+  type        = string
+}
